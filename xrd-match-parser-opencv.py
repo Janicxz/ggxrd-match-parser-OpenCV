d7acc8850e28c9b14114e76d3e92aaf9ff685086
Janizu99 <janizu99@gmail.com>
2018-04-28 05:02:46 +0300

    Works!

--- xrd-match-parser-opencv.py
+++ xrd-match-parser-opencv.py
@@ -1,5 +1,7 @@
 import os
 import datetime
+import argparse
+import subprocess
 
 import cv2
 import numpy as np
@@ -44,6 +46,8 @@ CHARACTER_MASKS = [
 ]
 VS_MASK = Mask('vs.png')
 
+FILE_OUTPUT = "matches.html"
+
 # Read the main image
 img_rgb = cv2.imread('test_image.png')
 
@@ -67,16 +71,62 @@ def matchTemplate(template, image, treshold):
 
 def format_timestamp(secs):
     return '[{}]'.format(
-        str(datetime.timedelta(seconds=int(sec))),
+        str(datetime.timedelta(seconds=int(secs))),
     )
+
+#URL = "https://www.youtube.com/watch?v=q3DY8EPtFMY"
+def writeToHtml(foundMatches, youtubeUrl):
+    htmlFile = open(FILE_OUTPUT, "w")
+    for match in foundMatches:
+        htmlFile.write('<a href={}#t={}>{} {} ({}) vs {} ({})</a><br>\n'.format(
+            youtubeUrl,
+            match.timeStamp,
+            format_timestamp(match.timeStamp),
+            match.charLeft,
+            match.playerOne,
+            match.charRight,
+            match.playerTwo,
+            ))
+    htmlFile.close()
 #def findPlayerName(filename):
 #     player_name = lambda s: os.path.basename(s).split('-')[0]
+
+
 
 videoFileName = "video.webm"
 SKIP_SECS = 20
 SEEK_SECS = 0.5
 if __name__ == '__main__':
     # https://www.youtube.com/watch?v=q3DY8EPtFMY test clip
+
+    parser = argparse.ArgumentParser(description="Parse XRD matches")
+    parser.add_argument(
+        'youtube_url',
+        type=str,
+        help='youtube video URL (e.g. https://www.youtube.com/watch?v=fOvG_TfnCVo)',
+    )
+    parser.add_argument(
+        '--already-downloaded',
+        action='store_true',
+        help='use existing youtube video file already on disk',
+    )
+    args = parser.parse_args()
+
+    # delete previous file
+    if os.path.exists(videoFileName) and not args.already_downloaded:
+        os.remove(videoFileName)
+
+    # download the video with youtube-dl
+    if not args.already_downloaded: 
+        subprocess.check_call(
+        'youtube-dl --format "bestvideo[height<=240]" --no-continue --output {} '
+        '"{}"'.format(
+            videoFileName,
+            args.youtube_url,
+        ),
+        shell=True,
+        )
+
     clip = VideoFileClip(videoFileName, audio=False)
     foundMatches = []
     next_sec = 0
@@ -94,9 +144,9 @@ if __name__ == '__main__':
 
         foundMatch = Match(0, "unknown","unknown","unknown","unknown")
         
+        #Find VS screen
         if matchTemplate(VS_MASK.template, clip_frame_gray, 0.8):
             #print("found VS")
-
             #Search for all characters
             ###########
             for char_mask in CHARACTER_MASKS:
@@ -157,9 +207,11 @@ if __name__ == '__main__':
             ))
             foundMatches.append(foundMatch)
 
-            cv2.imshow('Frame',clip_frame_bgr)
-            cv2.waitKey(0)
+           # cv2.imshow('Frame',clip_frame_bgr)
+            #cv2.waitKey(0)
             
             next_sec = sec + SKIP_SECS
         else:
             next_sec = sec + SEEK_SECS
+    print("Writing to html.")
+    writeToHtml(foundMatches, args.youtube_url)
